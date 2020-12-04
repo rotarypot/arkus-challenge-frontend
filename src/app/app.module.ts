d6e5ed91deb8fe9c454d23cda327e6c439b54f7b
Ulises Legarreta <rotarypot@gmail.com>
2020-12-03 17:28:57 -0800

    Adds bootstrap dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

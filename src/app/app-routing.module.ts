99bead69c7b7b337a5e5b8e3d79c0f65507e92f7
Ulises Legarreta <rotarypot@gmail.com>
2020-12-04 17:59:52 -0800

    Adds angular routing to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: UserListComponent },
+  { path: "user-edit/:id", component: UserEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
